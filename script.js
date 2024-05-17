f66f6269d05ed1c51b001f052199286737762a48
Aastik Sharma <137896880+0-aastik-sharma-0@users.noreply.github.com>
2024-05-17 16:38:42 +0530

    script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var love = document.querySelector("i");
 con.addEventListener("dblclick", function(){
     love.style.transform = 'translate(-50%,-50%) scale(1)';
     love.style.opacity = 1
-    love.style.color = 'red'
+    love.style.color = 'white'
 
     setTimeout(function(){
         love.style.opacity = 0
